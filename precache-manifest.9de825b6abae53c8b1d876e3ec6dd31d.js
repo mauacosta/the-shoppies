897cf0e6602ef6d76d00fae20990d31a3e02da27
Mau Acosta <chafito@live.com>
2020-09-05 20:50:29 -0500

    Updates

---
+++ precache-manifest.9de825b6abae53c8b1d876e3ec6dd31d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db0d2dccf082045dff4a45ea9900e048",
+    "url": "/the-shoppies/index.html"
+  },
+  {
+    "revision": "f0a1e2d19ad426384cd1",
+    "url": "/the-shoppies/static/css/main.ef3acd1d.chunk.css"
+  },
+  {
+    "revision": "80d1787a8a2a5bdb1ecf",
+    "url": "/the-shoppies/static/js/2.459d088a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/the-shoppies/static/js/2.459d088a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0a1e2d19ad426384cd1",
+    "url": "/the-shoppies/static/js/main.2c7a9ccf.chunk.js"
+  },
+  {
+    "revision": "3028237cd38fe8ab6725",
+    "url": "/the-shoppies/static/js/runtime-main.7a5fa803.js"
+  }
+]);
